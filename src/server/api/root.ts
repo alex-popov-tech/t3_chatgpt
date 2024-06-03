7f2dff13dd91cf4b17d8cfd0be93f3c54f063cea
Oleksandr Popov <alex.popov.tech@gmail.com>
2024-06-03 16:08:05 +0300

    update - delete obsolete post entity

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 import { conversationRouter } from "./routers/conversation";
 
@@ -8,7 +7,6 @@ import { conversationRouter } from "./routers/conversation";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   conversation: conversationRouter,
 });
 
